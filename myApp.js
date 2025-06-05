35c68eca209809f564d6adb552054034db96634c
VinceNguyen000 <vluxiphe@gmail.com>
2025-06-05 16:49:21 -0500

    fixed changes

--- myApp.js
+++ myApp.js
@@ -13,3 +13,61 @@ app.use(helmet.hsts({
   maxAge: 90 * 24 * 60 * 60, // can use variable/90 days in seconds
   force: true}
 ));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
+const api = require('./server.js');
+app.use(express.static('public'));
+app.disable('strict-transport-security');
+app.use('/_api', api);
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Your app is listening on port ${port}`);
+});
