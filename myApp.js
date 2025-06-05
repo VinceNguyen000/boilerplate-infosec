625378523a9c13026961cda54681296c4a93a79c
VinceNguyen000 <vluxiphe@gmail.com>
2025-06-05 16:45:17 -0500

    changes

--- myApp.js
+++ myApp.js
@@ -1,5 +1,15 @@
-const express = require("express");
-const helmet = require("helmet");
-const app = express();
+const express = require('express');
+const helmet = require('helmet');
+const app = express()
 
 app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({
+  action: 'deny'
+}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({
+  maxAge: 90 * 24 * 60 * 60, // can use variable/90 days in seconds
+  force: true}
+));
